aa7cd3996e22e7be5e76c62f09ad7f82da02e7da
kimani-25-kikis <kimanikikis@gmail.com>
2026-07-26 12:51:42 +0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,8 @@ Route::get('/sum/{num1}/{num2}', function(int $num1, int $num2){
     return "The sum of $num1 and $num2 is: $sum";
 })->whereNumber(['num1', 'num2']);
 
+Route::get('/add/{num1}/{num2?}', function (float $num1, float $num2 = 0) {
+    $sum = $num1 + $num2;
+    return "The sum of $num1 and $num2 is: $sum";
+})->where(['num1' => '[0-9]+(\.[0-9]+)?', 'num2' => '[0-9]+(\.[0-9]+)?']);
+
