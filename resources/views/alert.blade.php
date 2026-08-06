331a8566a0888de8b4c0eac2ee2db9f7b7dd5633
kimani-25-kikis <kimanikikis@gmail.com>
2026-08-06 11:24:20 +0300

    Challenged alert blade

---
+++ resources/views/alert.blade.php
@@ -0,0 +1,8 @@
+<div @style([
+    "color: $color" => true,
+    "padding: 15px",
+    "border: 1px solid currentColor",
+    "margin-bottom: 10px"
+])>
+    {{ $message }}
+</div>
--- resources/views/hello/welcome.blade.php
+++ resources/views/hello/welcome.blade.php
@@ -1,3 +1,27 @@
-<div>
+{{-- <div>
     {{ $name }} {{ $surname }}
-</div>
+</div> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Blade Challenge</title>
+</head>
+<body>
+
+    <h1>Welcome to My Website</h1>
+
+    <!-- Including the alert subview with custom variables -->
+    @include('alert', [
+        'message' => 'Success! Your changes have been saved completely.',
+        'color' => 'green'
+    ])
+
+    @include('alert', [
+        'message' => 'Congratulations Joshua',
+        'color' => 'blue'
+    ])
+
+</body>
+</html>
+
