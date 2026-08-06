558811646db0b72df9cb5a9467d65cb4b3e79bc0
kimani-25-kikis <kimanikikis@gmail.com>
2026-08-06 15:41:12 +0300

    Car selling web

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SignupController;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/hello', [HelloController::class, 'welcome']);
+Route::get('/signup', [SignupController::class, 'create']);
 
 
 
